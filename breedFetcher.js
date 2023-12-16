47ac78ad58fc8138084eb63400067ce3e1b9ff84
Kai Wingfield <kaiwingfield17@gmail.com>
2023-12-16 12:22:03 -0800

    refactor and testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,30 @@
 const request = require('request');
-let breedToSearch = process.argv[2]
 
-request(`https://api.thecattapi.com/v1/breeds/search?q=${breedToSearch}`, (error, response, body) => {
-  const data = JSON.parse(body);
-  if (error) {
-    return console.log(error);
-  }
-  if (data.length === 0) {
-    console.log("The requested breed cannot be found")
-  } else {
-  console.log(data);
-  }
-})
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    // if there is an error, sends error through callback and null for the description value
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+
+    const data = JSON.parse(body);
+    // if there is value for data[0], set desc to the value of description
+    let desc;
+    if (typeof data[0] === 'object') {
+      desc = data[0].description;
+    }
+
+    // if there is no data for data, return the specific error and null for description
+    if (data.length === 0) {
+      callback("The requested breed cannot be found", null);
+    } else {
+      callback(null, desc);
+    }
+    
+  });
+};
+
+module.exports = { fetchBreedDescription };
+
